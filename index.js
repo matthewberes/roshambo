96706feb1f9d4d40c66ec4c24fd316ccc2b21deb
matthewberes <matthewberes@trentu.ca>
2022-08-22 01:39:44 -0400

    implemented match system
    built a match system for best 2 of 3 and best 3 of 5, will track wins in a database in the future,
    
    added skeleton for login system

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-$(document).ready(function(){
+//Matt Beres, August 2022
+//simple rock paper scissors web app
 
+//will work with a mysql database named "roshambo" and a table named "userdata"
+//6 columns in the table so far: int usersId, varchar usersName, varchar usersEmail, varchar usersPwd, int usersBal, varchar usersDate
+
+//play rock paper scissors against a cpu
+//generates a random number with a range of 0-2 and determines who won with a switch statement
+//will give rewards for wins and keep track of winnings/losses and achievements
+
+$(document).ready(function(){
+	//initialize variables
+	let playingThree = 0;
+	let playingFive = 0;
+	let playerWins = 0;
+	let oppWins = 0;
+	let succGame = 0;
+	let disableShoot = 0;
+	document.getElementById("selectMove").value = "";
+	document.getElementById("selectGame").value = "";
+
+	//change user selection img
 	document.getElementById("selectMove").onchange = function() {displayPicP()};
 	function displayPicP(){
 		var thePic = "img/" + document.getElementById("selectMove").value + ".png";
 		document.getElementById("playerChoice").src = thePic;
 	}
 
+	//change opp selection img back to ?
 	document.getElementById("selectMove").onclick = function() {displayPicC()};
 	function displayPicC(){
 		document.getElementById("cpuChoice").src = "img/question.png";
-		console.log("IM RUNINNNN")
 	}
 
-
+	//roshambo
 	document.getElementById("shoot").onclick = function() { roll()};
 	function roll(){
 		//3 = win, 2 = draw, 1 = loss
@@ -25,6 +45,9 @@ $(document).ready(function(){
 			return
 		}
 
+		if(disableShoot == 1){
+			return
+		}
 		//show computers choice
 		switch(cpuChoice){
 			case 0:
@@ -88,17 +111,251 @@ $(document).ready(function(){
 				}
 			break;	
 		}
+		resultFunction(playerScore);
+	}
 
-		switch(playerScore){
-			case 1:
-				alert("you lost");
-			break;
-			case 2:
-				alert("draw");
-			break;
-			case 3:
-				alert("you won");
-			break;	
+	//account cell
+
+	//already have an account click
+	document.getElementById("aha").onclick = function() {AHA()};
+	function AHA(){
+		document.getElementById("accountTitle").innerHTML = "Log in";
+		var l = document.getElementById("logIn");
+		var s = document.getElementById("signUp");
+
+		l.style.display = "block";
+		s.style.display = "none";
+	}
+
+	//don't have account click
+	document.getElementById("dha").onclick = function() {DHA()};
+	function DHA(){
+		document.getElementById("accountTitle").innerHTML = "Sign up";
+		var l = document.getElementById("logIn");
+		var s = document.getElementById("signUp");
+
+		l.style.display = "none";
+		s.style.display = "block";
+	}
+
+	//play cell
+
+	//play match
+	document.getElementById("playBut").onclick = function() {PLAY()};
+	function PLAY(){
+		if(document.getElementById("selectGame").value == "three"){
+			var pThree = document.getElementById("playThree");
+			var pMenu = document.getElementById("playSelect");
+
+			pMenu.style.display = "none";
+			pThree.style.display = "block";
+			playingThree = 1;
+		}
+		else if(document.getElementById("selectGame").value == "five"){
+			var pFive = document.getElementById("playFive");
+			var pMenu = document.getElementById("playSelect");
+
+			pMenu.style.display = "none";
+			pFive.style.display = "block";
+			playingFive = 1;
+		}
+	}
+
+	//go back to play menu div
+	document.getElementById("replay").onclick = function() {AGAIN()};
+	function AGAIN(){
+		//reset ball colours for next game
+		if (playingThree == 1){
+			resetBalls(3);
+			playingThree = 0;
+		}
+		if (playingFive == 1){
+			resetBalls(5);
+			playingFive = 0;
+		}
+
+		//re-initialize variables, divs, and title
+		playerWins = 0;
+		oppWins = 0;
+		succGame = 0;
+		disableShoot = 0;
+		var pResult = document.getElementById("playResult");
+		var pMenu = document.getElementById("playSelect");
+		var pThree = document.getElementById("playThree");
+		var pFive = document.getElementById("playFive");
+
+		pResult.style.display = "none";
+		pMenu.style.display = "block";
+		pThree.style.display = "none";
+		pFive.style.display = "none";
+		document.getElementById("playTitle").innerHTML = "Play";
+	}
+
+	//track match wins
+	function resultFunction(num){
+		if(playingThree == 1){
+			tallyThree(num);
+		}
+		else if (playingFive == 1){
+			tallyFive(num);
+		}
+	}
+
+	//tallys wins for best 2 of 3
+	function tallyThree(num){
+		if(num == 1){
+			oppWins++;
+			succGame++;
+			colourBall(num);
+		}
+		else if(num == 3){
+			playerWins++;
+			succGame++;
+			colourBall(num);
+		}
+	}
+
+	//tallys wins for best 3 of 5
+	function tallyFive(num){
+		if(num == 1){
+			oppWins++;
+			succGame++;
+			colourBall(num);
+		}
+		else if(num == 3){
+			playerWins++;
+			succGame++;
+			colourBall(num);
+		}
+	}
+
+	//colours the balls to indicate wins and losses for the proper match type
+	function colourBall(score){
+		//if best 2 of 3
+		if(playingThree == 1){
+			switch(succGame){
+				case 1:
+				if(score == 1){
+					document.getElementById("dot13").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot13").style.backgroundColor = "green";
+				}
+				break;
+				case 2:
+				if(score == 1){
+					document.getElementById("dot23").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot23").style.backgroundColor = "green";
+				}
+				if(playerWins == 2){
+					whoWon(1,3);
+				}else if(oppWins == 2){
+					whoWon(2,3);
+				}
+				break;
+				case 3:
+				if(score == 1){
+					document.getElementById("dot33").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot33").style.backgroundColor = "green";
+				}
+				if(playerWins == 2,3){
+					whoWon(1,3);
+				}else if(oppWins == 2){
+					whoWon(2,3);
+				}
+				break;			
+			}			
+		}
+		//if best 3 of 5
+		else if(playingFive == 1){
+			switch(succGame){
+				case 1:
+				if(score == 1){
+					document.getElementById("dot15").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot15").style.backgroundColor = "green";
+				}
+				break;
+				case 2:
+				if(score == 1){
+					document.getElementById("dot25").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot25").style.backgroundColor = "green";
+				}
+				break;
+				case 3:
+				if(score == 1){
+					document.getElementById("dot35").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot35").style.backgroundColor = "green";
+				}
+				if(playerWins == 3){
+					whoWon(1,5);
+				}else if(oppWins == 3){
+					whoWon(2,5);
+				}
+				break;	
+				case 4:
+				if(score == 1){
+					document.getElementById("dot45").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot45").style.backgroundColor = "green";
+				}
+				if(playerWins == 3){
+					whoWon(1,5);
+				}else if(oppWins == 3){
+					whoWon(2,5);
+				}
+				break;
+				case 5:
+				if(score == 1){
+					document.getElementById("dot55").style.backgroundColor = "red";
+				}else if(score == 3){
+					document.getElementById("dot55").style.backgroundColor = "green";
+				}
+				if(playerWins == 3){
+					whoWon(1,5);
+				}else if(oppWins == 3){
+					whoWon(2,5);
+				}
+				break;	
+			}		
+		}
+	}
+
+	//switch to result div and update database(in future)
+	function whoWon(num, mode){
+		disableShoot = 1;
+		
+		if(num == 1){
+			document.getElementById("playTitle").innerHTML = "You won!";
+			var pResult = document.getElementById("playResult");
+			pResult.style.display = "block";
+		}
+		else if(num == 2){
+			document.getElementById("playTitle").innerHTML = "You lost!";
+			var pResult = document.getElementById("playResult");
+			pResult.style.display = "block";
+		}
+
+		if(mode == 3){//if win three
+		}else if(mode == 5){//if win five
+		}
+	}
+
+	//reset balls to grey for next game
+	function resetBalls(num){
+		if(num == 3){
+			document.getElementById("dot13").style.backgroundColor = "#bbb";
+			document.getElementById("dot23").style.backgroundColor = "#bbb";
+			document.getElementById("dot33").style.backgroundColor = "#bbb";
+		}else if(num == 5){
+			document.getElementById("dot15").style.backgroundColor = "#bbb";
+			document.getElementById("dot25").style.backgroundColor = "#bbb";
+			document.getElementById("dot35").style.backgroundColor = "#bbb";
+			document.getElementById("dot45").style.backgroundColor = "#bbb";
+			document.getElementById("dot55").style.backgroundColor = "#bbb";
 		}
 	}
 });
--- index.php
+++ index.php
@@ -34,14 +34,67 @@
 			</td>
 		</tr>
 		<tr>
-			<td>
-				<img style="width: 100%" src="img/testingimage.jpg"border=3/>
+			<td id="account" valign = top>
+				<h2 id="accountTitle" style="margin-top: 8px; margin-bottom: 8px">Sign up</h2>
+				<div id ="signUp" style="display: block">
+					<form>
+						<input type="text" placeholder="username..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="text" placeholder="email..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="password" placeholder="password..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="password" placeholder="repeat password..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="button" value="Submit"/>
+					</form>
+					<p id="aha" style="text-decoration: underline; margin-top: 4px; margin-bottom: 8px">Already have an account? Log in.</p>
+				</div>
+				<div id="logIn" style="display: none">
+					<form>
+						<input type="text" placeholder="username/email..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="password" placeholder="password..." style="width: 115px;  margin-bottom: 8px"/><br>
+						<input type="button" value="Log in"/>
+					</form>
+					<p id="dha" style="text-decoration: underline; margin-top: 4px; color: blue">Don't have an account? Sign up.</p>
+				</div>
+				<div id="loggedIn" style="display: none">
+					<form>
+						<input type="button" value="hello"/>
+					</form>
+				</div>
 			</td>
-			<td>
-				<img style="width: 100%" src="img/testingimage.jpg"border=3/>
+			<td id ="playCell" valign="top">
+				<h2 id="playTitle" style="margin-top: 8px; margin-bottom: 8px">Play</h2>
+				<div id="playSelect" style="display: block">
+					<label>Game type:</label><br>
+					<select class="gameList" id="selectGame" style="width: 75px; margin-bottom: 8px;">
+					<option value="" disabled selected>Select...</option>
+				    <option value="three">2/3</option>
+				    <option value="five">3/5</option>
+			    </select><br>
+			    <input type="button" id="playBut" name="playBut" value="Play!"/>
+				</div>
+				<div id="playThree" style="display: none">
+					<span id="dot13" class="dot"></span>
+					<span id="dot23" class="dot"></span>
+					<span id="dot33" class="dot"></span>
+				</div>
+				<div id="playFive" style="display: none">
+					<span id="dot15" class="dot"></span>
+					<span id="dot25" class="dot"></span>
+					<span id="dot35" class="dot"></span>
+					<span id="dot45" class="dot"></span>
+					<span id="dot55" class="dot"></span>
+				</div>
+				<div id="playResult" style="display: none">
+					<input type="button" id="replay" name="replay" value="Play again">
+				</div>
 			</td>
-			<td>
-				<img style="width: 100%" src="img/testingimage.jpg"border=3/>
+			<td id="stats" valign="top">
+				<h2 style="margin-top: 8px; margin-bottom: 8px">Statistics</h2>
+				<div id="statDefault" style="display: block">
+					<p>Log in to track your wins and achievements across sessions!</p>
+				</div>
+				<div id="statLogged" style="display: none">
+					<p></p>
+				</div>
 			</td>
 		</tr>
 	</table>
